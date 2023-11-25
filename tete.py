f77f2ea29d96d526452831f97b84a6f5177d0c1a
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-11-26 00:00:35 +0900

    Update tete.py

--- tete.py
+++ tete.py
@@ -38,3 +38,33 @@ for i in range(n):
         
 for i in dic:
    print(' '.join(dic[i]))
+
+
+
+def read_matrix(rows):
+    matrix = []
+    for _ in range(rows):
+        matrix.append(list(map(int, input().split())))
+    return matrix
+
+def add_matrices(matrix1, matrix2):
+    max_rows = max(len(matrix1), len(matrix2))
+    max_cols = max(max(len(row) for row in matrix1), max(len(row) for row in matrix2))
+
+    result = [[0 for _ in range(max_cols)] for _ in range(max_rows)]
+
+    for i in range(max_rows):
+        for j in range(max_cols):
+            val1 = matrix1[i][j] if i < len(matrix1) and j < len(matrix1[i]) else 0
+            val2 = matrix2[i][j] if i < len(matrix2) and j < len(matrix2[i]) else 0
+            result[i][j] = val1 + val2
+
+    return result
+
+m, n = map(int, input().split())
+matrix1 = read_matrix(m)
+matrix2 = read_matrix(n)
+
+result = add_matrices(matrix1, matrix2)
+for row in result:
+    print(row)
